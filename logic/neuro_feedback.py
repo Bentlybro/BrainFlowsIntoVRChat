100fdeabe233da6bc6b69bf26c4ac99df0ecf6bb
Bently <tomnoon9@gmail.com>
2024-10-18 19:14:08 +0100

    HR updates + other fixes

--- logic/neuro_feedback.py
+++ logic/neuro_feedback.py
@@ -33,10 +33,18 @@ class NeuroFB(PwrBands):
 
         # apply score calculations per location and add to ret_dict
         for nfb_name, nfb_func in function_dict.items():
-            signed_dict = {location + NeuroFB.SIGNED : nfb_func(location) for location in power_dict}
-            unsigned_dict = {location + NeuroFB.UNSIGNED : (value+1)/2 for location, value in signed_dict.items()}
+            left_value = nfb_func(PwrBands.LEFT)
+            right_value = nfb_func(PwrBands.RIGHT)
+            avg_value = (left_value + right_value) / 2
+
+            signed_dict = {
+                PwrBands.LEFT + NeuroFB.SIGNED: left_value,
+                PwrBands.RIGHT + NeuroFB.SIGNED: right_value,
+                PwrBands.AVERAGE + NeuroFB.SIGNED: avg_value
+            }
+            unsigned_dict = {key + NeuroFB.UNSIGNED: (value + 1) / 2 for key, value in signed_dict.items()}
             inner_flat_dict = signed_dict | unsigned_dict
-            inner_flat_dict = {nfb_name + key : value for key, value in inner_flat_dict.items()}
+            inner_flat_dict = {nfb_name + key: value for key, value in inner_flat_dict.items()}
             ret_dict |= inner_flat_dict
         
         return ret_dict
